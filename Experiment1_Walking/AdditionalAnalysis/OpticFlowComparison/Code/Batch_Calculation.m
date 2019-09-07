d66d55e5b4bd934fc0c9b6a3e5de10801dd17f88
dlcen <dlcen@protonmail.com>
2019-09-07 19:46:39 +0100

    Add the results of the numbers of motion parallax pairs in the code file.

--- Experiment1_Walking/AdditionalAnalysis/OpticFlowComparison/Code/Batch_Calculation.m
+++ Experiment1_Walking/AdditionalAnalysis/OpticFlowComparison/Code/Batch_Calculation.m
@@ -2,15 +2,19 @@
 clear all;
 thisCondition = 'Line'; 
 Cal_Flow_Info(thisCondition)
+% In this condition there are 0 pairs of motion parallax.
 
 clear all;
 thisCondition = 'Cloud'; 
 Cal_Flow_Info(thisCondition)
+% In the Cloud condition there are 34 pairs of motion parallax.
 
 clear all;
 thisCondition = 'Outline'; 
 Cal_Flow_Info(thisCondition)
+% In the Outline condition there are 0 pairs of motion parallax.
 
 clear all;
 thisCondition = 'Room'; 
 Cal_Flow_Info(thisCondition)
+% In the Room condition there are 0 pairs of motion parallax.
