bf1d0206d925da0a4e6b3e47166d8919654e4374
[dlcen] <[dlcen@protonmail.com]>
2020-02-23 11:28:19 +0000

    Plotted sampled dots in Cloud condition

--- Experiment1_Walking/AdditionalAnalysis/OpticFlowComparison/Cloud/DotSampling.m
+++ Experiment1_Walking/AdditionalAnalysis/OpticFlowComparison/Cloud/DotSampling.m
@@ -1,24 +1,50 @@
 
 %% Get the position of the dots
 cloud_dotPosition = readtable('Cloud_positions_6300.csv');
-cloud_dotPosition = table2array(dotPosition);
+cloud_dotPosition = table2array(cloud_dotPosition);
 
-% Get rid of those dots that are behind the viewer
-cloud_dotPosition = dotPosition(dotPosition(:, 3) > 0, :);
+cloud_dotPosition(:, 2) = cloud_dotPosition(:, 2) + 1.5;
+cloud_dotPosition(:, 3) = cloud_dotPosition(:, 3) + 1;
 
 % Label the dots: 0 - no restriction on the direction of flow vectors
 cloud_dotPosition = [cloud_dotPosition, zeros(size(cloud_dotPosition, 1), 1)];
 
 %% Add the sampled dots from the target post
-line_dotPosition = readtable('Line_position.csv');
+line_dotPosition = readtable('Line_positions.csv');
 line_dotPosition = table2array(line_dotPosition);
-
-% Set the position of the target sampled dots to be relative to the viewer
-line_dotPosition(:, 2) = line_dotPosition(:, 2) - 1.5;
-line_dotPosition(:, 3) = line_dotPosition(:, 3) - 1;
 
 %% Put the position of the cloud dots and target dots together
 dotPosition = [cloud_dotPosition; line_dotPosition];
+
+% Check the dot positions
+figure; scatter3(dotPosition(:, 1), dotPosition(:, 3), dotPosition(:, 2))
+
+%% Plot the sampled dots viewed from starting point ([0, 0]) at the height of 1.5m
+spv_x = dotPosition(:, 1)./dotPosition(:, 3);
+spv_y = (dotPosition(:, 2) - 1.5)./dotPosition(:, 3);
+
+fh = figure('Menu','none','ToolBar','none');
+ah = axes('Units','Normalize','Position',[0 0 1 1]);
+scatter(atand(spv_x), atand(spv_y), 1, 'filled')
+xlim([-45 45])
+ylim([-25 25])
+set(gca,'XTick',[]);
+set(gca,'YTick',[]);
+box on
+set(gcf, 'Units', 'centimeters', 'OuterPosition', [5, 5, 21, 14]);
+
+savefig(['SampledDots'])
+print(['SampledDots'], '-dsvg')
+
+%% Calculate the position of the sampled dots relative to the viewer
+dotPosition(:, 2) = dotPosition(:, 2) - 1.5;
+dotPosition(:, 3) = dotPosition(:, 3) - 1;
+
+% Remove the dots that are behind the viewer
+dotPosition = dotPosition(dotPosition(:, 3) > 0, :);
+
+% Check the dot positions
+figure; scatter3(dotPosition(:, 1), dotPosition(:, 3), dotPosition(:, 2))
 
 %% Save the position data
 save('dotPosition.mat', 'dotPosition')
