8f3e12fe84d401dfad2d54525b737dd4443100e8
[dlcen] <[dlcen@protonmail.com]>
2020-02-23 19:59:22 +0000

    Adjusted indent and added some comments to the code

--- Experiment1_Walking/AdditionalAnalysis/OpticFlowComparison/Code/Cal_Flow_Info.m
+++ Experiment1_Walking/AdditionalAnalysis/OpticFlowComparison/Code/Cal_Flow_Info.m
@@ -12,97 +12,97 @@
 
 function Cal_Flow_Info(thisCondition, dev_ang)
 
-	if nargin == 1
-		dev_ang = 10;
-	end
+if nargin == 1
+    dev_ang = 10;
+end
 
-	%% Set up the basic constant
-	dz = 1/37; % distance that the viewer has moved for the duration of one frame, starting from 6m from the target
+%% Set up the basic constant
+dz = 1/37; % distance that the viewer has moved for the duration of one frame, starting from 6m from the target
 
-	img_w = 90; % define the size of FoV, according to the specs of Oculus DK2
-	img_h = 50;
+img_w = 90; % define the size of FoV, according to the specs of Oculus DK2
+img_h = 50;
 
-	vf_x = tand(img_w/2);
-	vf_y = tand(img_h/2);
+vf_x = tand(img_w/2);
+vf_y = tand(img_h/2);
 
-	pF = 1/(6 - dz); % the distance to the target after one frame
+pF = 1/(6 - dz); % the distance to the target after one frame
 
-	Tr = [sind(dev_ang), 0, cosd(dev_ang)]; % calculating the transitional component
-	Rr = [0, -pF*Tr(1), 0]; % calculating the rotational component
+Tr = [sind(dev_ang), 0, cosd(dev_ang)]; % calculating the transitional component
+Rr = [0, -pF*Tr(1), 0]; % calculating the rotational component
 
-	%% Load the data
-	load(['../', thisCondition, '/dotPosition.mat'])
+%% Load the data
+load(['../', thisCondition, '/dotPosition.mat'])
 
-	%% Calculate and plot the flow field
-	%  Gaze to the direction of moving
-	image_data_Tr = Cal_Image_Vectors(dotPosition, Tr, 0, img_w, img_h);
+%% Calculate and plot the flow field
+%  Gaze to the direction of moving
+image_data_Tr = Cal_Image_Vectors(dotPosition, Tr, 0, img_w, img_h);
 
-	%  Gaze fixed on the target while moving
-	image_data_Rt = Cal_Image_Vectors(dotPosition, Tr, pF, img_w, img_h);
+%  Gaze fixed on the target while moving
+image_data_Rt = Cal_Image_Vectors(dotPosition, Tr, pF, img_w, img_h);
 
-	%% Plot the distribution of magnitude of speed vectors as a function of x-axis
-	image_data_Tr(:, 9) = abs(atand(image_data_Tr(:, 3)));
+%% Plot the distribution of magnitude of speed vectors as a function of x-axis
+image_data_Tr(:, 9) = abs(atand(image_data_Tr(:, 3)));
 
-	target_idx = find(image_data_Tr(:, 9) == 0 & image_data_Tr(:, 1) < 0.25/6 & image_data_Tr(:, 1) > -0.25/6);
+target_idx = find(image_data_Tr(:, 9) == 0 & image_data_Tr(:, 1) < 0.25/6 & image_data_Tr(:, 1) > -0.25/6);
 
-	target_data = image_data_Tr(target_idx, :);
-	[scene_data, ~] = setdiff(image_data_Tr, target_data, 'rows');
+target_data = image_data_Tr(target_idx, :);
+[scene_data, ~] = setdiff(image_data_Tr, target_data, 'rows');
 
-	figure
-	hold on
-	scatter3(atand(scene_data(:, 1)), atand(scene_data(:, 2)), scene_data(:, 9), 50, [.5 .5 .5], 'filled')
-	scatter3(atand(target_data(:, 1)), atand(target_data(:, 2)), target_data(:, 9), 50, [255/255, 191/255, 0], 'filled')
-	xlim([-img_w/2 img_w/2])
-	ylim([-img_h/2 img_h/2])
-	zlim([0 25])
-	set(gca, 'Ylabel', [])
-	set(gca, 'Xlabel', [])
-	set(gca, 'fontsize', 24)
-	view(0, 0)
-	set(gcf, 'Units', 'centimeters', 'OuterPosition', [5, 5, 21, 14]);
-	colorbar('off')
-	box on
-	grid off
-	set(gca, 'color', 'none')
-	set(gcf, 'color', 'none')
+figure
+hold on
+scatter3(atand(scene_data(:, 1)), atand(scene_data(:, 2)), scene_data(:, 9), 50, [.5 .5 .5], 'filled')
+scatter3(atand(target_data(:, 1)), atand(target_data(:, 2)), target_data(:, 9), 50, [255/255, 191/255, 0], 'filled')
+xlim([-img_w/2 img_w/2])
+ylim([-img_h/2 img_h/2])
+zlim([0 25])
+set(gca, 'Ylabel', [])
+set(gca, 'Xlabel', [])
+set(gca, 'fontsize', 24)
+view(0, 0)
+set(gcf, 'Units', 'centimeters', 'OuterPosition', [5, 5, 21, 14]);
+colorbar('off')
+box on
+grid off
+set(gca, 'color', 'none')
+set(gcf, 'color', 'none')
 
-	savefig(['../', thisCondition, '/SpeedMagnitudeDistribution'])
-	print(['../', thisCondition, '/SpeedMagnitudeDistribution'], '-dsvg')
+savefig(['../', thisCondition, '/SpeedMagnitudeDistribution'])
+print(['../', thisCondition, '/SpeedMagnitudeDistribution'], '-dsvg')
 
 
-	%% Calculate the motion parallax
-	closeness_threshold = 0.2 * sqrt(2); % define a distance threshold - under the threshold the two dots are so close to each other that they can be seen to be overlayed on the image plane.
+%% Calculate the motion parallax
+closeness_threshold = 0.2 * sqrt(2); % define a distance threshold - under the threshold the two dots are so close to each other that they can be seen to be overlayed on the image plane.
 
-	differential_data = Cal_motion_parallax_local_differential(image_data_Rt, tand(closeness_threshold));
+differential_data = Cal_motion_parallax_local_differential(image_data_Rt, tand(closeness_threshold));
 
-	fprintf('In the %s condition there are %d pairs of motion parallax.\n', thisCondition, size(differential_data, 1));
+fprintf('In the %s condition there are %d pairs of motion parallax.\n', thisCondition, size(differential_data, 1));
 
-	scale = 30;
-	cross_size = 2;
+scale = 30;
+cross_size = 2;
 
-	figure
-	hold on
-	box on
-	quiver(atand(image_data_Rt(:, 1)), atand(image_data_Rt(:, 2)), scale * image_data_Rt(:, 3), scale * image_data_Rt(:, 4), 0, 'Color', [.5 .5 .5], 'MaxHeadSize', .1, 'LineWidth', 1)
+figure
+hold on
+box on
+quiver(atand(image_data_Rt(:, 1)), atand(image_data_Rt(:, 2)), scale * image_data_Rt(:, 3), scale * image_data_Rt(:, 4), 0, 'Color', [.5 .5 .5], 'MaxHeadSize', .1, 'LineWidth', 1)
 
-	if size(differential_data, 1) > 1
-	    quiver(atand(differential_data(:, 1)), atand(differential_data(:, 2)), scale * differential_data(:, 3), scale * differential_data(:, 4), 0, 'Color', 'r', 'MaxHeadSize', 1, 'LineWidth', 2.5)
-	end
+if size(differential_data, 1) > 1
+    quiver(atand(differential_data(:, 1)), atand(differential_data(:, 2)), scale * differential_data(:, 3), scale * differential_data(:, 4), 0, 'Color', 'r', 'MaxHeadSize', 1, 'LineWidth', 2.5)
+end
 
-	gazeat = 0;
-	scatter(gazeat, 0, 150, [0, 0.4, 1], 'filled')
-	plot([dev_ang - cross_size; dev_ang + cross_size], [0; 0], 'color', [0.4078    0.6235    0.2196], 'LineWidth', 4)
-	plot([dev_ang; dev_ang], [-cross_size; cross_size], 'color', [0.4078    0.6235    0.2196], 'LineWidth', 4)
+gazeat = 0;
+scatter(gazeat, 0, 150, [0, 0.4, 1], 'filled')
+plot([dev_ang - cross_size; dev_ang + cross_size], [0; 0], 'color', [0.4078    0.6235    0.2196], 'LineWidth', 4)
+plot([dev_ang; dev_ang], [-cross_size; cross_size], 'color', [0.4078    0.6235    0.2196], 'LineWidth', 4)
 
-	xlim([-img_w/2 img_w/2])
-	ylim([-img_h/2 img_h/2])
-	set(gca, 'Ylabel', [])
-	set(gca, 'Xlabel', [])
-	set(gca, 'fontsize', 24)
-	set(gca,'color','none')
-	set(gcf, 'Units', 'centimeters', 'OuterPosition', [5, 5, 21, 14])
-	set(gca, 'color', 'none')
-	set(gcf, 'color', 'none')
+xlim([-img_w/2 img_w/2])
+ylim([-img_h/2 img_h/2])
+set(gca, 'Ylabel', [])
+set(gca, 'Xlabel', [])
+set(gca, 'fontsize', 24)
+set(gca,'color','none')
+set(gcf, 'Units', 'centimeters', 'OuterPosition', [5, 5, 21, 14])
+set(gca, 'color', 'none')
+set(gcf, 'color', 'none')
 
-	savefig(['../', thisCondition, '/FlowAndMotionParallaxField'])
-	print(['../', thisCondition, '/FlowAndMotionParallaxField'], '-dsvg')
+savefig(['../', thisCondition, '/FlowAndMotionParallaxField'])
+print(['../', thisCondition, '/FlowAndMotionParallaxField'], '-dsvg')
--- Experiment1_Walking/AdditionalAnalysis/OpticFlowComparison/Code/Cal_Image_Vectors.m
+++ Experiment1_Walking/AdditionalAnalysis/OpticFlowComparison/Code/Cal_Image_Vectors.m
@@ -12,56 +12,56 @@
 
 function image_data = Cal_Image_Vectors(env_dots, T,  pF, img_w, img_h)
 
-	nDots = size(env_dots, 1);
+nDots = size(env_dots, 1);
 
-	% Calculate the position of the dots on the image plane
-	image_x = env_dots(:, 1)./env_dots(:, 3);
-	image_y = env_dots(:, 2)./env_dots(:, 3);
+% Calculate the position of the dots on the image plane
+image_x = env_dots(:, 1)./env_dots(:, 3);
+image_y = env_dots(:, 2)./env_dots(:, 3);
 
-	% Calculate the velocity elements on the x-axis and y-axis on the image plane
-	image_vx = (env_dots(:, 1).* T(3) - env_dots(:, 3).* T(1) + T(1) * pF * (env_dots(:, 1).^2 + env_dots(:, 3).^2))./env_dots(:, 3).^2;
-	image_vy = (env_dots(:, 2).* T(3) - env_dots(:, 3).* T(2) + T(1) * pF * (env_dots(:, 1) .* env_dots(:, 2)))./env_dots(:, 3).^2;
+% Calculate the velocity elements on the x-axis and y-axis on the image plane
+image_vx = (env_dots(:, 1).* T(3) - env_dots(:, 3).* T(1) + T(1) * pF * (env_dots(:, 1).^2 + env_dots(:, 3).^2))./env_dots(:, 3).^2;
+image_vy = (env_dots(:, 2).* T(3) - env_dots(:, 3).* T(2) + T(1) * pF * (env_dots(:, 1) .* env_dots(:, 2)))./env_dots(:, 3).^2;
 
-  h_rst_id = find(env_dots(:, 4) == 1); 	% Find out those sampled dots along a horizontal line.
-  r_rst_id = find(env_dots(:, 4) == 2); 	% Find out those sampled dots along a vertical line.
+h_rst_id = find(env_dots(:, 4) == 1); 	% Find out those sampled dots along a horizontal line.
+r_rst_id = find(env_dots(:, 4) == 2); 	% Find out those sampled dots along a vertical line.
 
-  if ~isempty(h_rst_id)
-      image_vx(h_rst_id) = 0; 						% For those dots on a horizontal line, make the image speed on the x-axis to be 0.
-  end
+if ~isempty(h_rst_id)
+    image_vx(h_rst_id) = 0; 						% For those dots on a horizontal line, make the image speed on the x-axis to be 0.
+end
 
-  if ~isempty(r_rst_id)
-      image_vy(r_rst_id) = 0; 						% For those dots on a vertical line, make the image speed on the y-axis to be 0.
-  end
+if ~isempty(r_rst_id)
+    image_vy(r_rst_id) = 0; 						% For those dots on a vertical line, make the image speed on the y-axis to be 0.
+end
 
-	image_v  = sqrt(image_vx.^2 + image_vy.^2);
+image_v  = sqrt(image_vx.^2 + image_vy.^2);
 
-	% Calculate the direction of the velocity vector
-	image_dir = atand(image_vx./image_vy);
+% Calculate the direction of the velocity vector
+image_dir = atand(image_vx./image_vy);
 
-	% Make the value of the direction to be within the range [0, 360]
-	for i = 1:nDots
-		if image_vy(i) < 0
-			image_dir(i) = image_dir(i) + 180;
-		elseif image_vx(i) < 0
-			image_dir(i) = image_dir(i) + 360;
-		end
-	end
+% Make the value of the direction to be within the range [0, 360]
+for i = 1:nDots
+    if image_vy(i) < 0
+        image_dir(i) = image_dir(i) + 180;
+    elseif image_vx(i) < 0
+        image_dir(i) = image_dir(i) + 360;
+    end
+end
 
-	% Put together
-	image_data = NaN(nDots, 8);
+% Put together
+image_data = NaN(nDots, 8);
 
-	image_data(:, 1) = image_x;
-	image_data(:, 2) = image_y;
-	image_data(:, 3) = image_vx;
-	image_data(:, 4) = image_vy;
-	image_data(:, 5) = image_v;
-	image_data(:, 6) = image_dir;
-  image_data(:, 7) = env_dots(:, 4);
-  image_data(:, 8) = env_dots(:, 3);
+image_data(:, 1) = image_x;
+image_data(:, 2) = image_y;
+image_data(:, 3) = image_vx;
+image_data(:, 4) = image_vy;
+image_data(:, 5) = image_v;
+image_data(:, 6) = image_dir;
+image_data(:, 7) = env_dots(:, 4);
+image_data(:, 8) = env_dots(:, 3);
 
-	% Exclude those outside the vision
-	vf_x = tand(img_w/2); vf_y = tand(img_h/2);
+% Exclude those outside the vision
+vf_x = tand(img_w/2); vf_y = tand(img_h/2);
 
-	image_data = image_data(image_data(:, 1) >= -vf_x & image_data(:, 1) <= vf_x & image_data(:, 2) >= -vf_y & image_data(:, 2) <= vf_y, :);
+image_data = image_data(image_data(:, 1) >= -vf_x & image_data(:, 1) <= vf_x & image_data(:, 2) >= -vf_y & image_data(:, 2) <= vf_y, :);
 
 end
